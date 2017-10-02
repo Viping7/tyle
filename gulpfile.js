c2dda3b423ac6b2bc804a3748ff6ebc74311803d
Vipin <vipin.vipin.g498>
2017-10-02 13:35:34 +0530

    landing page and owl carousel removal

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,13 @@ var image=require('gulp-imagemin');
 var uglify=require('gulp-uglify');
 var gulpIf=require('gulp-if');
 var cssnano=require('gulp-cssnano');
+var jslint = require('gulp-jslint');
+var stylish = require('jshint-stylish');
+gulp.task('jslint', function () {
+   return gulp.src(['app/assets/js/tyle.js'])
+   .pipe(jslint({global: [ '$' ,'window','$(this)']}))
+		   .pipe(jslint.reporter( 'stylish' ));
+});
 gulp.task('browser-sync',function(){
 browserSync.init({
 	server:{
@@ -35,7 +42,13 @@ gulp.task('image', function(){
 gulp.task('useref',function(){
 	return gulp.src('app/*.html').pipe(useref()).pipe(gulpIf('*.js',uglify())).pipe(gulpIf('*.css',cssnano())).pipe(gulp.dest('docs')).pipe(browserSync.reload({stream:true}));
 });
-gulp.task('build',['useref','image']);
+gulp.task('jsbuild',function(){
+	return gulp.src('app/assets/js/tyle.js').pipe(gulp.dest('dist/js'));
+})
+gulp.task('cssbuild',function(){
+	return gulp.src('app/assets/css/tyle.css').pipe(gulp.dest('dist/css'));
+})
+gulp.task('build',['useref','image','jsbuild','cssbuild']);
 gulp.task('watch',['browser-sync','sass'],function(){
 gulp.watch('app/assets/scss/**/*.scss',['sass']);
 gulp.watch('app/*.html',browserSync.reload);
